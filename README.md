f06979b5b70682d519cac4ca7714a5c0f83b84d3
Shantanu Agarwal <shantanu.agarwal2109@gmail.com>
2025-05-15 23:36:29 -0400

    Updates

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ docker-compose up --build
 
 # Send a message
 
-curl -X POST localhost:8080/api/messages -H "Content-Type: text/plain" -d "Hello, Kafka!"
+curl -X POST localhost:8081/api/messages -H "Content-Type: text/plain" -d "Hello, Kafka!"
--- src/main/java/com/shanaga/kafka/demo/SpringKafkaDemoApplication.java
+++ src/main/java/com/shanaga/kafka/demo/SpringKafkaDemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringKafkaDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringKafkaDemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaDemoApplication.class, args);
+	}
 
 }
